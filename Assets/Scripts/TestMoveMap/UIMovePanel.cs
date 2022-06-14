247654a18bbd15428c1a5bdfc674186258de5bc7
SunnyShao <770238024@qq.com>
2022-06-14 11:17:27 +0800

    更新物体名字
    增加物体头顶名字

--- Assets/Scripts/TestMoveMap/UIMovePanel.cs
+++ Assets/Scripts/TestMoveMap/UIMovePanel.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class UIMovePanel : MonoBehaviour
 {
-    public GameObject followObj;
+    public GameObject[] followObj;
+    private GameObject moveUI;
+    public Camera uicamera;
+    public Vector3 offest;
+
+    private List<GameObject> moveUIList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveUI = transform.Find("MoveUI").gameObject;
+        for (int i = 0; i < followObj.Length; i++)
+        {
+            var obj = Instantiate(moveUI ,moveUI.transform.parent);
+            moveUIList.Add(obj);
+        }
+        moveUI.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        moveUIList.Clear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < followObj.Length; i++)
+        {
+            moveUIList[i].GetComponent<RectTransform>().anchoredPosition = GetUIPos(followObj[i].transform.position + offest);
+        }
+    }
+
+    private Vector2 GetUIPos(Vector3 worldPos)
+    {
+        Vector2 uiPos;
+        var screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPos);
+        uiPos = screenPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(moveUI.transform.parent.GetComponent<RectTransform>(), screenPoint, uicamera, out uiPos);
+        return uiPos;
     }
 }
