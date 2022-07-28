51ddbfb2dbce425ebadbb04358f32cf358906670
SunnyShao <770238024@qq.com>
2022-07-28 18:18:07 +0800

    Update UISphereLayout.cs
    提交：上次写的东西 忘记是啥了

--- Assets/Scripts/TestMoveMap/UISphereLayout.cs
+++ Assets/Scripts/TestMoveMap/UISphereLayout.cs
@@ -6,26 +6,26 @@ public class UISphereLayout : MonoBehaviour
 {
     public GameObject spawnObj;
     public int _count;
-    public float radius;
-    public float a;
+    [Header("半径")]
+    public float _radius;
+    [Header("平均份数")]
+    public float _average;
 
-    // Start is called before the first frame update
     void Start()
     {
         Vector2 zeroPos = spawnObj.GetComponent<RectTransform>().anchoredPosition;
-        for (int i = 0; i < 360; i++)
+        for (int i = 0; i < _count; i++)
         {
             var obj = Instantiate(spawnObj, transform);
-            //obj.GetComponent<RectTransform>().anchoredPosition = zeroPos - new Vector2(0, radius);
-
-            float x = zeroPos.x + radius * Mathf.Cos(i * Mathf.PI / 180);
-            float y = zeroPos.y + radius * Mathf.Sin(i * Mathf.PI / 180);
+            float rad = 360/ _average * i;
+            Debug.LogError(rad);
+            float x = zeroPos.x + _radius * Mathf.Cos(rad * Mathf.PI / 180);
+            float y = zeroPos.y + _radius * Mathf.Sin(rad * Mathf.PI / 180);
             Debug.Log(x + ":::" + y);
             obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y);
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
 
