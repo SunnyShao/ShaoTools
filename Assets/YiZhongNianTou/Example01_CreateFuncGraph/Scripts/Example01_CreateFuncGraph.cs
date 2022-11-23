a58bb5c203e70e4ebc6cf6ce61ed0b52bb3ef028
SunnyShao <770238024@qq.com>
2022-11-23 10:23:03 +0800

    更新：示例01准备

---
+++ Assets/YiZhongNianTou/Example01_CreateFuncGraph/Scripts/Example01_CreateFuncGraph.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example01_CreateFuncGraph : MonoBehaviour
+{
+    [Header("构建视图的预制体")]
+    public Transform pointPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
