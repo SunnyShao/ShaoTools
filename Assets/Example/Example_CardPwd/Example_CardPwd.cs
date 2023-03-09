f7fb8c3a8d54d35e54f78a191b5043fdb296004a
SunnyShao <770238024@qq.com>
2023-03-09 21:17:10 +0800

    测试

--- Assets/Example/Example_CardPwd/Example_CardPwd.cs
+++ Assets/Example/Example_CardPwd/Example_CardPwd.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class Example_CardPwd : MonoBehaviour
 {
-    private string url = "http://{0}:{1}";
+    private string url = "";
+    private string head = "http://{0}:{1}";
     private string ip = "120.53.248.67";
     private string port = "8180";
-    //private string url = "/capi/card/valid?cardId=cardkey4";
-    //private string url = "/capi/card/active?cardId=cardkey9&secret=12345678901234561&dvcKey=1112222";
+    private string activeUrl = "/capi/card/valid?cardId=cardkey4";
+    private string verifyUrl = "/capi/card/active?cardId=cardkey9&secret=12345678901234561&dvcKey=1112222";
+
+    private string active_CardId = "cardkey4";
+    private string verify_CardId = "cardkey9";
+    private string secret = "12345678901234561";
+    private string dvcKey = "1112222";
 
     // Start is called before the first frame update
     void Start()
     {
-        string all = Path.Combine(url, ip, port);
-        Debug.LogError(all);
+        url = string.Format(head, ip, port);
+        Debug.LogError(url);
     }
 
     private void PostMsg()
