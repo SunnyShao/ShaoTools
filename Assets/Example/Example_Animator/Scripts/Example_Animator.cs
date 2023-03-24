de14cd3d6a5426815f293bc87c60f95b718a8404
SunnyShao <770238024@qq.com>
2023-03-24 17:41:57 +0800

    更新：完善播放特效

--- Assets/Example/Example_Animator/Scripts/Example_Animator.cs
+++ Assets/Example/Example_Animator/Scripts/Example_Animator.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Example_Animator : MonoBehaviour
 {
+    private Animator _animator;
+
+    private const string test01 = "Test01";
+    private const string test02 = "Test02";
+
+    private void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,14 @@ public class Example_Animator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            _animator.SetTrigger(test01);
+        }
+
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            _animator.SetTrigger(test02);
+        }
     }
 }
