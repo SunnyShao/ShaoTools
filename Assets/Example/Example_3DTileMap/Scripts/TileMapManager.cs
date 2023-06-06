fa4ad269f6079dd40742d28ea0ced9cbab060777
SunnyShao <770238024@qq.com>
2023-06-06 16:46:01 +0800

    更新：练习瓦片api

--- Assets/Example/Example_3DTileMap/Scripts/TileMapManager.cs
+++ Assets/Example/Example_3DTileMap/Scripts/TileMapManager.cs
@@ -10,12 +10,26 @@ public class TileMapManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //清空全部Tile
         //curTimeMap.ClearAllTiles();
+
+        Debug.LogError(curTimeMap.CellToLocal(new Vector3Int(-8, 11, 0)));
+        Debug.LogError(curTimeMap.WorldToCell(new Vector3(-25.5f, 34.5f, 0f)));
+
+        //根据格子坐标获取瓦片
+        TileBase tile = curTimeMap.GetTile(new Vector3Int(-9, 11, 0));
+        Debug.LogError(tile);
+
+        //根据格子坐标设置瓦片
+        curTimeMap.SetTile(new Vector3Int(-10, 11, 0), tile);
+        
+        //根据格子坐标删除瓦片
+        //curTimeMap.SetTile(new Vector3Int(-9, 11, 0), null);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
