f4ed989c4de266c34ca80f3d74a5f9633b7fa8b6
SunnyShao <770238024@qq.com>
2023-06-07 18:32:42 +0800

    更新：资源位置调整

--- Assets/Example/Example_3DTileMap/Scripts/InstanceCube.cs
+++ Assets/Example/Example_3DTileMap/Scripts/InstanceCube.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InstanceCube : MonoBehaviour
@@ -33,15 +31,4 @@ public class InstanceCube : MonoBehaviour
     //}
 
     //_BlendColor
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
