7fbbef57a372e828416fe314a4047fa2d6cca02a
SunnyShao <770238024@qq.com>
2023-06-09 16:24:17 +0800

    更新：第一种血条

--- Assets/CodeMonkey/Example00_HealthBarDamageTaken/Scripts/HealthBarFade.cs
+++ Assets/CodeMonkey/Example00_HealthBarDamageTaken/Scripts/HealthBarFade.cs
@@ -4,13 +4,17 @@ using UnityEngine.UI;
 
 public class HealthBarFade : MonoBehaviour
 {
-    private const float DAMAGED_HEALTH_FADE_TIMER_MAX = 1f;  
+    private const float DAMAGED_HEALTH_FADE_TIMER_MAX = 1f;  //渐变计时器上限
 
     private Image barImage;
     private Image damagedBarImage;
+    private Button addButton;
+    private Button subtractionButton;
+
+    // 缓存颜色
     private Color damagedColor;
 
-    //渐变时间
+    //渐变时间 当计时器小于0时，降低alpha值
     private float damagedHealthFadeTimer;
 
     private HealthSystem healthSystem;
@@ -19,6 +23,12 @@ public class HealthBarFade : MonoBehaviour
     {
         barImage = transform.Find("bar").GetComponent<Image>();
         damagedBarImage = transform.Find("damagedBar").GetComponent<Image>();
+
+        addButton = transform.Find("AddButton").GetComponent<Button>();
+        addButton.onClick.AddListener(OnAddButtonClick);
+        subtractionButton = transform.Find("SubtractionButton").GetComponent<Button>();
+        subtractionButton.onClick.AddListener(OnSubtarctionBtnClick);
+
         damagedColor = damagedBarImage.color;
         damagedColor.a = 0f;
         damagedBarImage.color = damagedColor;
@@ -32,6 +42,21 @@ public class HealthBarFade : MonoBehaviour
         healthSystem.OnHealed += HealthSystem_OnHealed;
     }
 
+    private void Update()
+    {
+        // 当受伤进度条显示出来时
+        if (damagedColor.a > 0f)
+        {
+            // 受伤进度条显示指定的时间后 受伤进度条渐隐
+            damagedHealthFadeTimer -= Time.deltaTime;
+            if(damagedHealthFadeTimer < 0)
+            {
+                damagedColor.a -= 5f * Time.deltaTime;
+                damagedBarImage.color = damagedColor;
+            }
+        }
+    }
+
     private void HealthSystem_OnHealed(object sender, EventArgs e)
     {
         SetHealth(healthSystem.GetHealthNormalized());
@@ -39,11 +64,42 @@ public class HealthBarFade : MonoBehaviour
 
     private void HealthSystem_OnDamaged(object sender, EventArgs e)
     {
+        if (damagedColor.a <= 0f)
+        {
+            //Damage bar Image is invisible
+            damagedBarImage.fillAmount = barImage.fillAmount;
+        }
+
+        damagedColor.a = 1;
+        damagedBarImage.color = damagedColor;
+        damagedHealthFadeTimer = DAMAGED_HEALTH_FADE_TIMER_MAX;
+
         SetHealth(healthSystem.GetHealthNormalized());
     }
 
     private void SetHealth(float health)
     {
         barImage.fillAmount = health;
+    }
+
+    private void OnSubtarctionBtnClick()
+    {
+        healthSystem.Damage(10);
+    }
+
+    private void OnAddButtonClick()
+    {
+        healthSystem.Heal(10);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log(healthSystem == null);
+        healthSystem.OnDamaged -= HealthSystem_OnDamaged;
+        healthSystem.OnHealed -= HealthSystem_OnHealed;
+        healthSystem = null;
+
+        addButton.onClick.RemoveAllListeners();
+        subtractionButton.onClick.RemoveAllListeners();
     }
 }
